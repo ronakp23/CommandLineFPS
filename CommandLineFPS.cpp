6929c94e2ec01b6185f9934f7b824a5cfa5428c9
Ronak Patel <ronakp2023@gmail.com>
2023-10-09 21:02:31 -0400

    Added Display Map

--- CommandLineFPS.cpp
+++ CommandLineFPS.cpp
@@ -136,24 +136,22 @@ int main() {
 
 			// Shader walls based on distance
 			short nShade = ' ';
-			if (fDistanceToWall <= fDepth / 4.0f)			nShade = 0x2588;	// Very close	
+			if (fDistanceToWall <= fDepth / 4.0f)			nShade = 0x2588;	
 			else if (fDistanceToWall < fDepth / 3.0f)		nShade = 0x2593;
 			else if (fDistanceToWall < fDepth / 2.0f)		nShade = 0x2592;
 			else if (fDistanceToWall < fDepth)				nShade = 0x2591;
-			else											nShade = ' ';		// Too far away
+			else											nShade = ' ';
 
-			if (bBoundary)		nShade = ' '; // Black it out
+			if (bBoundary)		nShade = ' ';
 			
 			for (int y = 0; y < nScreenHeight; y++)
 			{
-				// Each Row
 				if(y <= nCeiling)
 					screen[y*nScreenWidth + x] = ' ';
 				else if(y > nCeiling && y <= nFloor)
 					screen[y*nScreenWidth + x] = nShade;
-				else // Floor
+				else
 				{				
-					// Shade floor based on distance
 					float b = 1.0f - (((float)y -nScreenHeight/2.0f) / ((float)nScreenHeight / 2.0f));
 					if (b < 0.25)		nShade = '#';
 					else if (b < 0.5)	nShade = 'x';
@@ -167,6 +165,18 @@ int main() {
 
         }
 
+        // Display Stats
+		swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f ", fPlayerX, fPlayerY, fPlayerA, 1.0f/fElapsedTime);
+
+		// Display Map
+		for (int nx = 0; nx < nMapWidth; nx++)
+			for (int ny = 0; ny < nMapWidth; ny++)
+			{
+				screen[(ny+1)*nScreenWidth + nx] = map[ny * nMapWidth + nx];
+			}
+		screen[((int)fPlayerX+1) * nScreenWidth + (int)fPlayerY] = 'P';
+
+        // Display Frame
         screen[nScreenWidth * nScreenHeight - 1] = '\0';
 		WriteConsoleOutputCharacterW(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
         
